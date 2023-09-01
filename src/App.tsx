f532152f6491d342c29623983d0e1fc9872bcf6b
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-09-01 21:34:28 +0200

    Add Navbar and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,23 @@ import Navbar from "./components/Navbar.tsx";
 import Contact from "./pages/Contact.jsx";
 import BadURL from "./pages/BadURL.jsx";
 import { Route, Routes } from "react-router-dom";
+import Cart from "./pages/Cart.tsx";
+import Footer from "./components/Footer.tsx";
+import { useState, useEffect } from "react";
+
 const App = () => {
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+  const isPhone = windowWidth < 769;
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
       <Navbar />
@@ -11,8 +27,10 @@ const App = () => {
         <Route path="/" element={<div>123</div>} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
+        <Route path="cart" element={<Cart />}></Route>
         <Route path="*" element={<BadURL />} />
       </Routes>
+      {isPhone && <Footer />}
     </>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,25 @@
-
 import { Link } from "react-router-dom";
+import { CartIcon } from "../assets/carticon.jsx";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/">
+      <Link to="/" className="navbar-name">
         <h2>Phone Paradise</h2>
       </Link>
 
-      <div>
-        <Link to="/about">Why us?</Link>
-        <Link to="/contact">Contact</Link>
-      </div>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/about">Why us?</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/cart">
+            <CartIcon />
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };

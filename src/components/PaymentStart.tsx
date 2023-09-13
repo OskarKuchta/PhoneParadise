41a28a698dfa6e90fdbeefdac2e7c53fb64bdf97
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-09-13 19:14:36 +0200

    Add base to html

--- src/components/PaymentStart.tsx
+++ src/components/PaymentStart.tsx
@@ -1,5 +1,21 @@
 import PaymentTop from "./PaymentTop";
+import { useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+import { NavigateFunction } from "react-router";
 const PaymentStart = () => {
+  const [count, setCount] = useState<number>(10);
+  const navigate: NavigateFunction = useNavigate();
+  useEffect(() => {
+    const intervalId: React.ReactNode = setInterval(() => {
+      setCount((prevCount) => prevCount - 1);
+    }, 1000);
+    if (count === 0) {
+      navigate("/");
+    }
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [count, navigate]);
   return (
     <section className="payment-start">
       <PaymentTop />
@@ -8,7 +24,7 @@ const PaymentStart = () => {
           You will be redirected to your bank's website shortly. To proceed,
           click the button below or wait a few seconds.
         </h3>
-        <button>To payment</button>
+        <button className="payments-start-button">To payment</button>
       </div>
     </section>
   );

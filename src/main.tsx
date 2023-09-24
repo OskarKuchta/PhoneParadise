3e7aedc9fdafa3eb9fc53c5f223b53469d7b7882
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-09-24 21:06:35 +0200

    Try to set persist

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import store from "./store";
+import store, { Persistor } from "./store";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={Persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store.tsx
+++ src/store.tsx
@@ -1,19 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import productsReducer from "./features/Productslice.tsx";
 import { productsFetch } from "./features/Productslice.tsx";
 import CartReducer from "./features/CartSlice.tsx";
 import ModalReducer from "./features/ModalSlice.tsx";
 import PaymentReducer from "./features/PaymentSlice.tsx";
-const store = configureStore({
-  reducer: {
-    products: productsReducer,
-    cart: CartReducer,
-    modal: ModalReducer,
-    payment: PaymentReducer,
-  },
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "main-root",
+  storage,
+};
+const rootReducer = combineReducers({
+  products: productsReducer,
+  cart: CartReducer,
+  modal: ModalReducer,
+  payment: PaymentReducer,
 });
-
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (applyMiddleware) => applyMiddleware(),
+});
 store.dispatch(productsFetch());
-
+const Persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
+export { Persistor };
 export default store;

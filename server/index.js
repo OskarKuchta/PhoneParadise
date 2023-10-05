848d1484c5957b42cf787c9caf854b2385d87d71
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-10-05 18:57:51 +0200

    Remove mongoDB, need to find another base

--- server/index.js
+++ server/index.js
@@ -1,70 +1,28 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import serverless from "serverless-http";
 import products from "../data/products.js";
-import { MongoClient } from "mongodb";
 
 
-const url = "mongodb+srv://Oskar98:kuchta123@cluster0.bsb9bp8.mongodb.net/"
 
-const app = express();
-const router = express.Router();
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-const databaseName = 'rating';
-const collectionName = 'ratings';
-
-const database = client.db(databaseName);
-const collection = database.collection(collectionName);
-
-async function connectToDatabase() {
-    try {
-        await client.connect();
-        console.log('Connected to the database');
-    } catch (error) {
-        console.error('Error connecting to the database', error);
-    }
-
-}
-
-connectToDatabase();
 app.use(express.json())
 
 router.use(cors({
     origin: "*"
 }));
 
-const ratingSchema = new mongoose.Schema({
-    rating: Number,
+router.get("/vote", (req, res) => {
+    res.send(ratings)
+})
+router.post("/vote", (req, res) => {
+    const { rate } = req.body;
+    ratings.default.push({
+        date: new Date(),
+        rate: rate,
+    });
+    res.status(201).json({ message: "Vote added successfully" });
 });
 
-const Rating = mongoose.model('Rating', ratingSchema);
-
-
-router.post("/vote", async (req, res) => {
-    const rating = req.body.rating;
-
-    try {
-        const newRating = new Rating({ rating });
-        await newRating.save();
-        res.json({ success: true });
-    } catch (error) {
-        console.error('Error saving rating to the database', error);
-        res.status(500).json({ success: false });
-    }
-});
-
-router.get("/vote", async (req, res) => {
-
-    try {
-        const result = await collection.find({}).toArray();
-        console.log('Fetched data:', result);
-        res.send(result);
-    } catch (error) {
-        console.error('Error fetching data', error);
-        res.status(500).json("Porblem przy pobraniu bazy");
-    }
-});
 router.get("/products", (req, res) => {
     res.send(products)
 })

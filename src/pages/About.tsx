3baf5484be71f9a19b3d1401ab40cd3058964603
Oskar Kuchta <oskarkuchta5@gmail.com>
2024-01-03 20:02:51 +0100

    Add motion variants for desktop

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,29 +6,32 @@ import { AnimationObject } from "../Types/Types";
 import CountUp from "react-countup";
 import { CartIcon, EyeIcon, Smileicon } from "../assets/icons";
 const About: FC = () => {
-  const fadeInAnimation: AnimationObject = {
+  const fadeLeftAnimation: AnimationObject = {
     hidden: { opacity: 0, x: -50 },
     visible: { opacity: 1, x: 0 },
   };
-
+  const fadeUpAnimation: AnimationObject = {
+    hidden: { opacity: 0, y: -50 },
+    visible: { opacity: 1, y: 0 },
+  };
   return (
     <>
       <main className="m-8 flex flex-col items-center md:flex-row md:items-start">
         <div className="w-full md:w-1/2">
           <motion.h2
-            variants={fadeInAnimation}
+            variants={fadeLeftAnimation}
             initial="hidden"
             animate="visible"
-            transition={{ duration: 0.7 }}
+            transition={{ duration: 1 }}
             className="text-purple my-8 text-center md:text-start"
           >
             Why Choose Phone Paradise?
           </motion.h2>
           <motion.p
-            variants={fadeInAnimation}
+            variants={fadeLeftAnimation}
             initial="hidden"
             animate="visible"
-            transition={{ duration: 0.5, delay: 0.7 }}
+            transition={{ duration: 0.7, delay: 1.2 }}
           >
             Phone Paradise is where your dreams of the perfect phone come to
             life. For years, our company has been delivering the best
@@ -36,10 +39,10 @@ const About: FC = () => {
             why you should choose our store:
           </motion.p>
           <motion.ul
-            variants={fadeInAnimation}
+            variants={fadeLeftAnimation}
             initial="hidden"
             animate="visible"
-            transition={{ duration: 0.5, delay: 1.2 }}
+            transition={{ duration: 0.7, delay: 2 }}
             className="list-none mt-8"
           >
             <li className="my-6">
@@ -76,24 +79,54 @@ const About: FC = () => {
           </motion.ul>
         </div>
         <div className="w-full md:w-1/2 flex flex-row justify-center items-center md:ml-16 mt-12 mb-20 md:h-[60vh] gap-16 flex-wrap text-purple">
-          <div className="relative w-48 h-48 border-2 border-purple  rounded flex flex-col items-center p-6">
+          <motion.div
+            className="relative w-48 h-48 border-2 border-purple  rounded flex flex-col items-center p-6"
+            variants={fadeUpAnimation}
+            initial="hidden"
+            animate="visible"
+            transition={{ duration: 0.7, delay: 2.5 }}
+          >
             <div className="absolute bottom-0 w-full h-4 bg-purple"></div>
             <Smileicon />
-            <CountUp end={24.5} suffix=" K" decimals={1} className="mt-4" />
+            <CountUp
+              end={24.5}
+              suffix=" K"
+              decimals={1}
+              className="mt-4"
+              delay={2.5}
+            />
             <p className="text-center">satisfied customers</p>
-          </div>
-          <div className="relative w-48 h-48 border-2 border-purple rounded flex flex-col items-center p-6">
+          </motion.div>
+          <motion.div
+            className="relative w-48 h-48 border-2 border-purple rounded flex flex-col items-center p-6"
+            variants={fadeUpAnimation}
+            initial="hidden"
+            animate="visible"
+            transition={{ duration: 0.7, delay: 3 }}
+          >
             <div className="absolute bottom-0 w-full h-4 bg-purple"></div>
             <CartIcon color="rgb(46, 3, 87)" width={48} height={48} />
-            <CountUp end={185} suffix=" K" className="mt-4" />
+            <CountUp end={185} suffix=" K" className="mt-4" delay={3} />
             <p className="text-center">orders realized</p>
-          </div>
-          <div className="relative w-48 h-48 border-2 border-purple rounded flex flex-col items-center p-6">
+          </motion.div>
+          <motion.div
+            className="relative w-48 h-48 border-2 border-purple rounded flex flex-col items-center p-6"
+            variants={fadeUpAnimation}
+            initial="hidden"
+            animate="visible"
+            transition={{ duration: 0.7, delay: 3.5 }}
+          >
             <div className="absolute bottom-0 w-full h-4 bg-purple"></div>
             <EyeIcon />
-            <CountUp end={2.5} suffix=" M" decimals={1} className="mt-4" />
+            <CountUp
+              end={2.5}
+              suffix=" M"
+              decimals={1}
+              className="mt-4"
+              delay={3.5}
+            />
             <p className="text-center">times page visited</p>
-          </div>
+          </motion.div>
         </div>
       </main>
       <FooterDesktop />

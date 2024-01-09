4090fd055ee037aed59f8b2c518b526bb5e9592c
Oskar Kuchta <oskarkuchta5@gmail.com>
2024-01-09 10:35:32 +0100

    Add i18n to project

--- src/components/Footer/DesktopFooter.tsx
+++ src/components/Footer/DesktopFooter.tsx
@@ -1,8 +1,12 @@
 import { FC } from "react";
+import { useTranslation } from "react-i18next";
+
 const DesktopFooter: FC = () => {
+  const { t } = useTranslation();
+
   return (
     <footer className="hidden md:block fixed bottom-0 right-0 left-0 w-screen text-center bg-gray-800 text-lightGray p-4">
-      <p>Phone Paradise &#169; 2023 – Your Source for the Latest Technology</p>
+      <p>Phone Paradise &#169; 2023 – {t("desktop-footer")}</p>
     </footer>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,9 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { PhoneProvider } from "./context/PhoneProvider";
+import "./i18n.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={Persistor}>

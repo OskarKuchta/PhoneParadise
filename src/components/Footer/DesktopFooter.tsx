4a97c9fb6c2fe7a13e49438c05a8dd828085d3d9
Oskar <oskarkuchta5@gmail.com>
2024-01-16 14:05:48 +0100

    Change height of footer

--- src/components/Footer/DesktopFooter.tsx
+++ src/components/Footer/DesktopFooter.tsx
@@ -5,7 +5,7 @@ const DesktopFooter: FC = () => {
   const { t } = useTranslation();
 
   return (
-    <footer className="hidden md:block fixed bottom-0 right-0 left-0 w-screen text-center bg-gray-800 text-lightGray p-4">
+    <footer className="hidden md:block fixed bottom-0 right-0 left-0 w-screen text-center bg-gray-800 text-lightGray p-2">
       <p>Phone Paradise &#169; 2024 – {t("desktop-footer")}</p>
     </footer>
   );
